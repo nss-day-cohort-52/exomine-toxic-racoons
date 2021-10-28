a32582c90fc104df04bc606d498c727dcc4a50d7
ChasityBrown <chasity.brown@asurion.com>
2021-10-28 16:51:39 -0400

    working on the .find method to find the government object

--- scripts/colonies.js
+++ scripts/colonies.js
@@ -1,21 +1,42 @@
-import { getColonies, getcolonyMinerals, setGovernor } from "./database.js" 
+import { getColonies, getMinerals, getGovernor, getColonyMinerals } from "./database.js" 
+import { coloniesGovernor } from "./govenors.js"
 //importing copies of the array of colonies to be used and referenced 
 //to generate and display in HTML
 // const colonies = getColonies()
 // const colonyMinerals = getcolonyMinerals()
-const setgovernor = setGovernor()
-//setting a variable with the value of the function that makes a copy of the colonies
-
-document.addEventListener(//needs to display colonies that chosen governor governs
-    "change",
-    (changeEvent) => {
-        if (changeEvent.target.id === "governor") {
-            const chosenOption = changeEvent.target.value
-            const setColony = parseInt(chosenOption.id)
-            return setColony
+const governors = getGovernor()
+const colonies = getColonies()
+const minerals = getMinerals()
+const colonyMineral = getColonyMinerals()
+const buildColonyListItem = (colony) => {
+    const foundGovernor = governors.find(
+        (governor) => {
+            return governor.id === colony.id
         }
-    }
-)
+    )
+const governorChosen = coloniesGovernor()
+//setting a variable with the value of the function that makes a copy of the colonies
+}
+
+
+
+
+export const Colony = () => {
+    let html = ""
+
+    for ()
+
+}
+// document.addEventListener(//needs to display colonies that chosen governor governs
+//     "change",
+//     (changeEvent) => {
+//         if (changeEvent.target.id === "governor") {
+//             const chosenOption = changeEvent.target.value
+//             const setColony = parseInt(chosenOption.id)
+//             return setColony
+//         }
+//     }
+// )
 export const ColonyMinerals = () => { //created a variable whose value is a function
     let html = "<ul>" //unordered list
 
