5f96940bc733cc4803de2111e3ad8ad8190d5322
ChasityBrown <chasity.brown@asurion.com>
2021-10-29 16:13:23 -0400

    cleaned up the colonies module and commented all of the code

--- scripts/colonies.js
+++ scripts/colonies.js
@@ -1,14 +1,14 @@
 import { getColonies, getMinerals, getGovernors, getColonyMinerals, setGovernor, getSelectedGovernor } from "./database.js" 
-import { coloniesGovernor } from "./govenors.js"
+//import { coloniesGovernor } from "./govenors.js"
 //importing copies of the array of colonies to be used and referenced 
 //to generate and display in HTML
 // const colonies = getColonies()
 // const colonyMinerals = getcolonyMinerals()
-const governors = getGovernors()
+// const governors = getGovernors()
 const colonies = getColonies()
 const minerals = getMinerals()
 const colonyMinerals = getColonyMinerals()
-const governorChosen = coloniesGovernor()
+//const governorChosen = coloniesGovernor()
 //setting a variable with the value of the function that makes a copy of the colonies
 //const foundColony = 
 
@@ -16,68 +16,47 @@ const governorChosen = coloniesGovernor()
 
 
 export const ColonyHTML = () => {    
-    const chosenOption = getSelectedGovernor()
-    
-    if (chosenOption.governorId !== 0) {
-        const governors = getGovernors()
+    const chosenOption = getSelectedGovernor() 
+    // variable catches the transient state of the orderbuilder object
+    if (chosenOption.governorId !== 0) { 
+        //if the governorId  of the chosen governor is not zero then execute the following code
+        const governors = getGovernors() //variable whose value is a function that
+        //returns a copy of the governors array
 
         const foundGovernor = governors.find(
             (governor) => {
                 return chosenOption.governorId === governor.id
             }
+            //the above function uses the chosenOption/transient state/ orderbuilder's governorId 
+            //to use the find method through the array of governors to compare with primary key
+            // the value found is the  found governor value which is an object
         )
             const foundColony = colonies.find((colony) => {
                 return colony.id === foundGovernor.colonyId
             }
             )
+            //the above function uses the foundgovernor object to grab the colonyId and compare it
+            //to the colony array to get the value of the corresponding colony to the governor 
+
             const foundcolonyMineral = colonyMinerals.find((colonyMineral) => {
                 return colonyMineral.colonyId === foundColony.id
             })
+            //the above function uses the found colony primary key and compares it to the colonyMinerals array
+            //this returns the corresponding colonyMineral object
+
             const foundMineral = minerals.find((mineral) => {
                 return mineral.id === foundcolonyMineral.mineralId
             })
+            //the above mentioned function uses the found colonyMineral object and compares the mineralId from it
+            //to the mineral primary key in the minerals array. This returns the corresponding mineral object
+
             let html = `${foundColony.name} needs ${foundcolonyMineral.amount} tons of
             ${foundMineral.mineral}`
             return html
+            //the above mentioned code returns the html which interpolates the value of the found 
+            //colony name and mineral name after all above code is executed properly
     } else {
         return `Colonies and Needed Minerals`
-
-    }
-    
-    
-    
-//     colonies.map(colony => {
-//         if (colonies.colonyid === governorChosen.colonyid) {
-//             return `<li>
-//                 ${colony.name}
-//                 ${colonyMineral.name}
-//                 </li>`
-                
-//             }
-            
-//         })
-//         html += ""
-//         return html
-
-// }
-// document.addEventListener(//needs to display colonies that chosen governor governs
-//     "change",
-//     (changeEvent) => {
-//         if (changeEvent.target.id === "governor") {
-//             const chosenOption = changeEvent.target.value
-//             const setColony = parseInt(chosenOption.id)
-//             return setColony
-//         }
-//     }
-// )
-// export const ColonyMinerals = () => { //created a variable whose value is a function
-//     let html = "<ul>" //unordered list
-
-//     // This is how you have been converting objects to <li> elements
-//     for (const colonyMineral of colonyMinerals) { //this function will iterate through the  colonies array
-//         //and display a list of colonies in the html
-//         html += `<li>
-//             <input type="radio" name="colony" value="${colonyMineral.id}" /> ${colony.name}
-//         </li>`
-//     
+        //this code happens if a governor is not selected
+    }   
 }
