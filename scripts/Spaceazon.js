cd8ebb83d512ad6e24c66320a122e1eb4c356145
andy.eilers <andy.eilers@asurion.com>
2021-10-29 13:16:52 -0500

    added orders

--- scripts/Spaceazon.js
+++ scripts/Spaceazon.js
@@ -1,4 +1,35 @@
 //Generating HTML represenetation of order state
 
 // Importing functions from database module
-import { getOrders, getFacilities, getFacilityMetals}
+import { getOrders, getFacilityMinerals } from "./database.js"
+
+const facilityMinerals = getFacilityMinerals()
+
+const buildOrderListItem = (order) => {
+    const foundFacilityMineral = facilityMinerals.find(
+        (facilityMineral) => {
+            return facilityMineral.id === order.facilityMineralId
+        }
+    )
+
+    const totalAmount = foundFacilityMineral.amount
+
+    const amountString = totalAmount(int)
+
+    return `<li>
+    Order #${order.id} amount ${amountString}
+    </li>`
+}
+
+export const Orders = () => {
+    const orders = getOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
